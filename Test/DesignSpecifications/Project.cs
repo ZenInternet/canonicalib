27997403f327ab687bd2bba4e2211b6437d81a32
Ben Wolstencroft <ripzay@gmail.com>
2025-10-30 09:46:05 +0000

    bug: fix typo

--- Test/DesignSpecifications/Project.cs
+++ Test/DesignSpecifications/Project.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCanonicaLib(() =>
-    new Zen.CanonicaLib.UI.WebApplicationOptions()
+    new CanonicaLibOptions()
     {
         PageTitle = "Zen's Canonical Libraries"
     }
